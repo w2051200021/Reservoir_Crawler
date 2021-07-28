733e02ec357a274179ade1c9e23b36c145c7ce75
w2051200021 <66375899+w2051200021@users.noreply.github.com>
2021-07-28 22:18:09 +0800

    new figure
    Higher resolution

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import crawler
 
-target = "德基水庫"
-dir = r"C:\Users\user\Documents\GitHub\Reservoir_Crawler\data\Deji_Reservoir"
-
-start_time = [2003, 1, 1]
-end_time = [2004, 7, 28]
+target = "石門水庫"
+# dir = r"C:\Users\user\Documents\GitHub\Reservoir_Crawler\data\Deji_Reservoir"
+dir = r"C:\Users\user\Desktop\碩一\碩一下\Python practices"
 
 crawler = crawler.Crawler(
             reservoir = target,
             file_dir = dir
         )
+
+start_time = [2003, 1, 1]
+end_time = [2020, 12, 31]
 
 crawler.get_data(
             start_time = start_time,
